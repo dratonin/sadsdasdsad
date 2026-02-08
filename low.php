e718439903987f32d008643577ff866c1eaa74c9
Daniel <143292584+dratonin@users.noreply.github.com>
2026-02-08 13:56:02 +0100

    fix(security): AutoFix vulnerability - fix-low.php:10:php.lang.security.injection.tainted-sql-string.tainted-sql-string-1770555281867

--- low.php
+++ low.php
@@ -7,19 +7,27 @@ if( isset( $_REQUEST[ 'Submit' ] ) ) {
 	switch ($_DVWA['SQLI_DB']) {
 		case MYSQL:
 			// Check database
-			$query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
-			$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
+			// Use prepared statement to prevent SQL injection
+			$stmt = mysqli_prepare($GLOBALS["___mysqli_ston"], "SELECT first_name, last_name FROM users WHERE user_id = ?");
+			if ($stmt) {
+				// Bind the parameter as integer
+				mysqli_stmt_bind_param($stmt, "i", $id);
+				mysqli_stmt_execute($stmt);
+				$result = mysqli_stmt_get_result($stmt);
+				
+				// Get results
+				while( $row = mysqli_fetch_assoc( $result ) ) {
+					// Get values
+					$first = $row["first_name"];
+					$last  = $row["last_name"];
 
-			// Get results
-			while( $row = mysqli_fetch_assoc( $result ) ) {
-				// Get values
-				$first = $row["first_name"];
-				$last  = $row["last_name"];
-
-				// Feedback for end user
-				$html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
+					// Feedback for end user
+					$html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
+				}
+				
+				mysqli_stmt_close($stmt);
 			}
-
+			
 			mysqli_close($GLOBALS["___mysqli_ston"]);
 			break;
 		case SQLITE:
@@ -28,26 +36,35 @@ if( isset( $_REQUEST[ 'Submit' ] ) ) {
 			#$sqlite_db_connection = new SQLite3($_DVWA['SQLITE_DB']);
 			#$sqlite_db_connection->enableExceptions(true);
 
-			$query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
-			#print $query;
-			try {
-				$results = $sqlite_db_connection->query($query);
-			} catch (Exception $e) {
-				echo 'Caught exception: ' . $e->getMessage();
-				exit();
-			}
-
-			if ($results) {
-				while ($row = $results->fetchArray()) {
-					// Get values
-					$first = $row["first_name"];
-					$last  = $row["last_name"];
-
-					// Feedback for end user
-					$html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
+			// Check database
+			// Use prepared statement to prevent SQL injection
+			$stmt = $sqlite_db_connection->prepare("SELECT first_name, last_name FROM users WHERE user_id = :id");
+			if ($stmt) {
+				// Bind the parameter
+				$stmt->bindValue(':id', $id, SQLITE3_INTEGER);
+				
+				try {
+					$results = $stmt->execute();
+				} catch (Exception $e) {
+					echo 'Caught exception: ' . $e->getMessage();
+					exit();
 				}
-			} else {
-				echo "Error in fetch ".$sqlite_db->lastErrorMsg();
+
+				// Get results
+				if ($results) {
+					while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
+						// Get values
+						$first = $row["first_name"];
+						$last  = $row["last_name"];
+
+						// Feedback for end user
+						$html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
+					}
+				} else {
+					echo "Error in fetch ".$sqlite_db_connection->lastErrorMsg();
+				}
+				
+				$stmt->close();
 			}
 			break;
 	} 
